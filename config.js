27d44b8c22dbb7eab1e582d0e929ad7fb4d99d56
Nicolas <nicolas@Mac-mini-de-Nicolas.local>
2019-05-28 16:39:52 -0500

    Agregar env variables

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'secret': 'secret_token',
     'authorizationError': 'No autorizado',
-    'database':  'mongodb://localhost/Test0'
+    'database':  process.env.MONGODB_URI || 'mongodb://localhost/Test0'
 }
 
 // mongodb+srv://dbUser:ESCAMAS@apicluster-dizmk.mongodb.net/test?retryWrites=true
